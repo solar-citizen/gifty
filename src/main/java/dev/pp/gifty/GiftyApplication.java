6c7ea965771fc125726d1fdeaad0d7d42920bb30
Vladyslav Tishchenko <fsknnn@gmail.com>
2023-03-09 02:07:41 +0200

    Add CORS - add CORS filter and Web Config

--- src/main/java/dev/pp/gifty/GiftyApplication.java
+++ src/main/java/dev/pp/gifty/GiftyApplication.java
@@ -2,8 +2,10 @@ package dev.pp.gifty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class GiftyApplication {
     public static void main(String[] args) {
         SpringApplication.run(GiftyApplication.class, args);
---
+++ src/main/java/dev/pp/gifty/WebConfig.java
@@ -0,0 +1,19 @@
+package dev.pp.gifty;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<CORSFilter> corsFilter() {
+        FilterRegistrationBean<CORSFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CORSFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setName("CORS Filter");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
+    }
+}
